ae6f46605a6523f2123b441a10f2bc2d7af13b77
hackerxwang <hackerxwang@163.com>
2017-09-07 09:12:45 +0800

    Visualization of Fibonacci sequence

--- Visualization_of_Fibonacci_sequence.py
+++ Visualization_of_Fibonacci_sequence.py
@@ -2,11 +2,11 @@ import time
 import matplotlib.pyplot as plt 
 
 
-def rabbit(n):
+def rabbit(n):  #非递归算法
 	if n==1 or n==2:
 		return 1
 	else:
-		a = [None]*(n+1)  #列表
+		a = [None]*(n+1)  #生成空列表
 		a[0] = 0
 		a[1] = 1
 		for i in range(2,n+1):
@@ -25,9 +25,9 @@ def rabbit(n):
 y = [None]*6
 y[0] = 0
 k = 1
-#for j in range(1,n+1): #生成1~250
+
 for j in [1,250,500,750,1000]:
-	start = time.clock()
+	start = time.clock() #计算时间
 	sum = rabbit(j)
 	print("第",j,"年的兔子数:",sum) 
 	end = time.clock()
@@ -35,10 +35,9 @@ for j in [1,250,500,750,1000]:
 	print()
 	y[k] = (end-start)*1000
 	k = k+1
-# for i in range(n+1):
-# 	x[i] = i 
 x = [0,1,250,500,750,1000]
-#y = [1,2,3,4,5]
+
+#绘制图表
 plt.xlabel("year")
 plt.ylabel("ms")
 plt.title("Visualization of Fibonacci sequence")
